395f841e61dc9a5b884b5490689b5bb8a7e2e329
Jerry Bai <jerrybai1@hotmail.com>
2018-04-10 18:33:47 +0800

    first iteration, recieving servo triggers from android app

--- PuzzleBoxController.ino
+++ PuzzleBoxController.ino
@@ -1,20 +1,55 @@
+int led = D7;
+int pin1 = D0;
+int pin2 = D1;
+int pin3 = D2;
 
-//-------------------------------
-// VARIABLES
-//-------------------------------
+int pos = 0;
+Servo myservo;
 
-
-//-------------------------------
-// SETUP
-//-------------------------------
 void setup()
 {
+
+   pinMode(led, OUTPUT);
+
+   Particle.function("servo",servoToggle);
+
+   digitalWrite(led, LOW);
+
 }
 
-//-------------------------------
-// MAIN FUNCTIONS
-//-------------------------------
 void loop()
 {
+  delay(1);
+}
 
+
+int servoToggle(String servoNum) {
+
+    int currPin;
+
+    if (servoNum == "1") {
+        currPin = pin1;
+    } else if (servoNum == "2") {
+        currPin = pin2;
+    } else {
+        currPin = pin3;
+    }
+
+    myservo.attach(currPin);
+
+    for(pos = 90; pos>=0; pos-=1) {
+        myservo.write(pos);
+        delay(15);
+    }
+
+    delay(1000);
+
+     for(pos = 0; pos < 90; pos += 1) {
+        myservo.write(pos);
+        delay(15);
+    }
+
+    myservo.detach();
+
+    return 1;
 }
